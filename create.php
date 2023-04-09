1f0d31de8b270a41f47c0a2ef10f2fb9c053a5db
Aiman2017 <50306902+Aiman2017@users.noreply.github.com>
2023-04-09 10:14:32 +0300

    Update create.php

--- create.php
+++ create.php
@@ -2,43 +2,47 @@
 require_once 'db.php';
 $con = connect();
 $errors = [];
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $price = $_POST['price'];
-        $date = date('Y-m-d H:i:s');
-        if ($title == '' || $price == '') {
-            $errors['error'] = 'some field is not inserted';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $date = date('Y-m-d H:i:s');
+    $image = $_FILES['image'] ?? null;
+    $imagePath = 'images/' . 'IMG-' . $image['name'];
+
+    if ($title == '' || $price == '') {
+        $errors[] = 'some field is not inserted';
+    }
+
+    if (!is_dir('images')) {
+        mkdir('images');
+    }
+    if ($image['size'] > 125000) {
+        $errors[] = 'The file is too large';
+    }
+    $imgEx = pathinfo($image['name'], PATHINFO_EXTENSION);
+    $imgExtension = ['jpg', 'png', 'jpeg'];
+    if (!in_array($imgEx, $imgExtension) && !empty($image['name'])) {
+        $errors[] = 'The file can\'\t be accept';
+    }
+    if (empty($errors)) {
+        if ($image) {
+            move_uploaded_file($image['tmp_name'], $imagePath);
         }
+        $stm = $con->prepare("INSERT INTO product (title, description, price, image, create_date) VALUES (:title, :description, :price, :image, :date)");
 
-        if (!is_dir('images')) {
-            mkdir('images');
-        }
-        if (empty($errors)) {
-            $image = $_FILES['image'] ?? null;
-            $imagePath = '';
-//             if the image is exists, move it to another file and adding unique name
-            if ($image && $image['name']) {
-                $imagePath = 'images/' . randomString(10). '/' . $image['name'];
-//              moving the uploading images in currently path
-                mkdir(dirname($imagePath));
-                move_uploaded_file($image['tmp_name'],$imagePath);
-            }
-
-            $stm = $con->prepare("INSERT INTO product (title, description, price, image, create_date) VALUES (:title, :description, :price, :image, :date)");
-
-            $stm->bindValue(':title', $title);
-            $stm->bindValue(':description', $description);
-            $stm->bindValue(':price', $price);
-            $stm->bindValue(':image', $imagePath);
-            $stm->bindValue(':date', $date);
-            $check = $stm->execute();
-            if ($check !== false) {
-                redirect('index');
-                exit();
-            }
+        $stm->bindValue(':title', $title);
+        $stm->bindValue(':description', $description);
+        $stm->bindValue(':price', $price);
+        $stm->bindValue(':image', $imagePath);
+        $stm->bindValue(':date', $date);
+        $check = $stm->execute();
+        if ($check !== false) {
+            redirect('index');
+            exit();
         }
     }
+}
 
 ?>
 
@@ -51,9 +55,9 @@ $errors = [];
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <style>
-       * {
-           padding:10px;
-       }
+        * {
+            padding: 10px;
+        }
     </style>
     <title>Document</title>
 </head>
@@ -64,22 +68,25 @@ $errors = [];
         <label>Product Image</label><br>
         <input type="file" name="image">
     </div>
-        <?php if (!empty($errors)) :?>
-            <div class="alert alert-danger" role="alert">
-                <?= $errors['error']?>
-            </div>
-         <?php endif;?>
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?php foreach ($errors as $error): ?>
+                <?= $error . '<br>' ?>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
     <div class="form-group">
         <label for="title">title</label>
-        <input type="text" class="form-control" value="<?= $title ?? ''?>" name="title" id="title" placeholder="title">
+        <input type="text" class="form-control" value="<?= $title ?? '' ?>" name="title" id="title" placeholder="title">
     </div>
     <div class="form-group">
         <label for="description">description</label>
-        <textarea class="form-control" name="description"  id="description"  placeholder="description"><?= $description ??''?></textarea>
+        <textarea class="form-control" name="description" id="description"
+                  placeholder="description"><?= $description ?? '' ?></textarea>
     </div>
     <div class="form-group">
-        <label  for="price">price</label>
-        <input type="number" step="0.1" class="form-control"  value="<?= $price ?? ''?>" name="price" id="price">
+        <label for="price">price</label>
+        <input type="number" step="0.1" class="form-control" value="<?= $price ?? '' ?>" name="price" id="price">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
